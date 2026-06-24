c6ab2b30e2e2e23ca779a79df7275fd6aedc8916
Anastasia Yerashova <yerashova.a@gmail.com>
2026-06-24 13:09:01 +0200

    add Enrollment

--- src/main/java/university/entities/Enrollment.java
+++ src/main/java/university/entities/Enrollment.java
@@ -1,4 +1,55 @@
 package university.entities;
+import university.enums.Grade;
+import university.interfaces.Payable;
 
-public class Enrollment {
+public class Enrollment implements Payable {
+    private Student student;
+    private Course course;
+    private String semester;
+    private Grade grade;
+    private boolean paid;
+
+    public Enrollment(Student student, Course course, String semester) {
+        this.student = student;
+        this.course = course;
+        this.semester = semester;
+        this.grade = Grade.NA;
+        this.paid = false;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    @Override
+    public boolean isPaid() {
+        return paid;
+    }
+
+    @Override
+    public String toString() {
+        return "Студент: " + student.getName() + " | Курс: " + course.getTitle() +
+                " | Семестр: " + semester + " | Оцінка: " + grade + " | Оплачено: " + (paid ? "Так" : "Ні");
+    }
 }
