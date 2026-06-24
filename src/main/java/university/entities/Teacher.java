779056f57b3fc8388c6982f8c7495ec2fdeffc88
Anastasia Yerashova <yerashova.a@gmail.com>
2026-06-24 12:51:45 +0200

    add Teacher

--- src/main/java/university/entities/Teacher.java
+++ src/main/java/university/entities/Teacher.java
@@ -1,4 +1,24 @@
 package university.entities;
+import university.enums.TeacherPosition;
 
-public class Teacher {
+public class Teacher extends Person {
+    private TeacherPosition position;
+
+    public Teacher(int id, String name, String email, TeacherPosition position) {
+        super(id, name, email);
+        this.position = position;
+    }
+
+    public TeacherPosition getPosition() {
+        return position;
+    }
+
+    public void setPosition(TeacherPosition position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Посада: " + position;
+    }
 }
