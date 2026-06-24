2e13059ea287092b5250983c738a0bb8c634d934
Anastasia Yerashova <yerashova.a@gmail.com>
2026-06-24 12:39:46 +0200

    add Person

--- src/main/java/university/entities/Person.java
+++ src/main/java/university/entities/Person.java
@@ -1,4 +1,44 @@
 package university.entities;
 
-public class Person {
+public abstract class Person {
+    private int id;
+    private String name;
+    private String email;
+
+    public Person(int id, String name, String email) {
+        if (!email.contains("@")) {
+            throw new IllegalArgumentException("Помилка: неправильний формат email.");
+        }
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (!email.contains("@")) {
+            throw new IllegalArgumentException("Помилка: неправильний формат email.");
+        }
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Ім'я: " + name + ", Email: " + email;
+    }
 }
